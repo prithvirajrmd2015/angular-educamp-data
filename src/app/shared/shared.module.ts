7e8bff8af41cc1543dcdaee2e962c1fc7f4c036b
prithvirajrmd2015 <prithvirajrmd@gmail.com>
2020-11-10 02:37:23 +0530

    2 files modified

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,6 +1,4 @@
 import { NgModule } from "@angular/core";
-import { CommonModule } from "@angular/common";
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { MenubarModule } from "primeng/menubar";
 import { InputTextModule } from "primeng/inputtext";
 import { ButtonModule } from "primeng/button";
